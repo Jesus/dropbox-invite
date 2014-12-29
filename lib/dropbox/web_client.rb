d428c71951c6b756b281eb49e1cfcca355ddd841
Jesús Burgos <jesus@visrez.com>
2014-12-29 22:53:35 +0100

    Clean up.

--- lib/dropbox/web_client.rb
+++ lib/dropbox/web_client.rb
@@ -5,16 +5,3 @@ require 'dropbox/web_client/authentication'
 require 'dropbox/web_client/cookie_manager'
 require 'dropbox/web_client/actions'
 require 'dropbox/web_client/session'
-
-
-=begin
-require 'dropbox-api'
-
-# Require our own classes
-require 'dropbox/dropbox-invite/util/config'
-require 'dropbox/dropbox-invite/util/response_parser'
-
-# Require our class extensions
-require 'dropbox/dropbox-invite/client'
-require 'dropbox/dropbox-invite/objects/dir'
-=end
--- lib/dropbox/web_client/parser_utils.rb
+++
@@ -1,7 +0,0 @@
-def try_block(default_value = nil)
-  begin
-    yield
-  rescue
-    return default_value
-  end
-end
