ca2c3a687dbe0dacfb77da86e1386436eb198a10
Jesús Burgos <jesus@visrez.com>
2014-12-30 14:19:44 +0100

    Reorganized tests.

--- spec/actions_spec.rb
+++ spec/actions_spec.rb
@@ -1,14 +1,13 @@
 describe Dropbox::WebClient::Actions do
-  describe "invite" do    
-    let(:session) do
-      Dropbox::WebClient::Session.new(
-        :email => "c2553105@trbvm.com",
-        :password => "c2551055@trbvm.comc2551055@trbvm.com"
-      )
-    end
 
-    # We've recorded VCR cassettes for the following situations on a Dropbox
-    # account.
+  let(:session) do
+    Dropbox::WebClient::Session.new(
+      :email => "c2553105@trbvm.com",
+      :password => "c2551055@trbvm.comc2551055@trbvm.com"
+    )
+  end
+
+  describe "invite" do
     it "fails if no folder found", :cassette => "invite_unexisting_folder" do
       r = session.invite("/peanuts", ["test1@somewhere.com"])
       
@@ -32,4 +31,22 @@ describe Dropbox::WebClient::Actions do
     end
  
   end
+
+  describe "share_options" do    
+
+    it "fails if no folder found", :cassette => "share_opts_unexisting" do
+      r = session.share_options("/peanuts")
+
+      expect(r.error?).to be_truthy
+    end
+    
+    it "works on already shared folders", :cassette => "share_opts_shared" do
+      r = session.share_options("/shared folder")
+
+      expect(r.response_data[:members].map{|m|m[:email]})
+        to match_array(["c2553105@trbvm.com", "test1@somewhere.com"])
+    end
+
+  end
+
 end
