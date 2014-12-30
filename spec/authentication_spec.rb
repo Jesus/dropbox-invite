bf02fa9d91f0bfb26eeb5fcb6bc0c715ea706150
Jesús Burgos <jesus@visrez.com>
2014-12-30 13:46:03 +0100

    Update spec's organization to be more coherent.

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -1,17 +1,13 @@
-describe Dropbox::WebClient do
-  describe "authentication" do
+describe Dropbox::WebClient::Authentication do
+  it "starts a web session on Dropbox website",
+    :cassette => "login" do
+    session = Dropbox::WebClient::Session.new(
+      :email => "c2553105@trbvm.com",
+      :password => "c2551055@trbvm.comc2551055@trbvm.com"
+    )
+    session.authenticate
     
-    it "starts a web session on Dropbox website",
-      :cassette => "login" do
-      session = Dropbox::WebClient::Session.new(
-        :email => "c2553105@trbvm.com",
-        :password => "c2551055@trbvm.comc2551055@trbvm.com"
-      )
-      session.authenticate
-      
-      
-      expect(session.cookies.login_token).to eq("r-gLoiDMnJ98MBj-xzTCJ-gL")
-    end
- 
+    
+    expect(session.cookies.login_token).to eq("r-gLoiDMnJ98MBj-xzTCJ-gL")
   end
 end
